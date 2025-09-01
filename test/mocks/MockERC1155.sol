ea2d95c83e32402f215a8725e3fadef52b876f9d
Sajad <SajadSolidity@gmail.com>
2025-09-01 19:16:24 +0330

    Added mock erc1155

---
+++ test/mocks/MockERC1155.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract MockERC1155 is ERC1155 {
+    constructor(string memory uri_) ERC1155(uri_) {}
+
+    /// @notice Mint tokens to an address
+    function mint(address to, uint256 id, uint256 amount, bytes memory data) external {
+        _mint(to, id, amount, data);
+    }
+
+}
