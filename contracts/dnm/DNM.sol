728d5a8ac205777c0f987e405e08e96d440e91af
Farbod Shams <farbodshams.2000@gmail.com>
2025-09-24 15:39:20 +0330

    Added DNM Contract

---
+++ contracts/dnm/DNM.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DecentralizedNetworkMarketingV2 is ERC20, Ownable {
+    mapping(address => bool) public isMintOperator;
+
+    constructor(address recipient, uint256 initialSupply)
+        ERC20("Decentralized Network Marketing V2", "DNM")
+        Ownable(msg.sender)
+    {
+        _mint(recipient, initialSupply * 10 ** decimals());
+    }
+
+    function setMintOperator(address _operator, bool _isMintOperator) public onlyOwner {
+        isMintOperator[_operator] = _isMintOperator;
+    }
+
+    modifier onlyMindOperator {
+        require(isMintOperator[msg.sender], "Only mint operator can mint");
+        _;
+    }
+
+    function mint(address to, uint256 amount) public onlyMindOperator {
+        _mint(to, amount);
+    }
+}
