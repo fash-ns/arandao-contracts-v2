6438186d3cc48c98e331e40a140fa0fc7babd2d9
Sajad <SajadSolidity@gmail.com>
2025-10-02 19:46:37 +0330

    updated

--- src/Vault/VaultCore/SwapHelper.sol
+++ src/Vault/VaultCore/SwapHelper.sol
@@ -53,6 +53,36 @@ abstract contract SwapHelper is VaultStorage {
     }
 
     /**
+     * @notice Executes a swap from a source token (tokenIn) to the base token (DAI), ensuring an exact amount of DAI is received.
+     * @dev Uses Uniswap V2 Router's swapTokensForExactTokensSupportingFeeOnTransferTokens.
+     * @param tokenIn The address of the token to sell.
+     * @param amountOut The exact amount of DAI to receive.
+     * @param amountInMax The maximum amount of tokenIn to spend.
+     * @param to The address that will receive the resulting DAI tokens.
+     */
+    function _swapForExactDAI(address tokenIn, uint256 amountOut, uint256 amountInMax, address to) internal {
+        require(tokenIn != DAI, "Token is already DAI");
+        require(amountOut > 0, "Amount out must be greater than zero");
+
+        uint256 deadline = block.timestamp + _deadlineDuration;
+
+        // Approve the router using forceApprove for secure allowance set (using amountInMax)
+        IERC20(tokenIn).forceApprove(address(_uniswapRouter), amountInMax);
+
+        // Build path: tokenIn -> DAI
+        address[] memory path = new address[](2);
+        path[0] = tokenIn;
+        path[1] = DAI;
+
+        // Perform swap
+        // Note: swapTokensForExactTokens requires amountOut as the first argument
+        _uniswapRouter.swapTokensForExactTokens(amountOut, amountInMax, path, to, deadline);
+
+        // Reset allowance securely
+        IERC20(tokenIn).forceApprove(address(_uniswapRouter), 0);
+    }
+
+    /**
      * @notice Executes a swap from a source token (tokenIn) to the base token (DAI).
      * @param tokenIn The address of the token to sell.
      * @param amountIn The exact amount of tokenIn to sell.
