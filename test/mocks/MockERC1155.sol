2ee630aa9567306314fe64d5e60f737dfe305da7
Sajad <SajadSolidity@gmail.com>
2025-11-02 20:21:07 +0330

    update

--- test/mocks/MockERC1155.sol
+++ test/mocks/MockERC1155.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract MockERC1155 is ERC1155 {
     constructor(string memory uri_) ERC1155(uri_) {}
