181d0853fce23d59c59cf35123d701ed1824dc81
Sajad <SajadSolidity@gmail.com>
2025-11-02 23:55:51 +0330

    update

--- test/Vault/SwapHelper.t.sol
+++ test/Vault/SwapHelper.t.sol
@@ -1,160 +1,160 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.30;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
 
-// import "forge-std/Test.sol";
-// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
-// import {IQuoter} from "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
-// import {SwapHelper} from "../../src/Vault/VaultCore/SwapHelper.sol";
-// import {VaultStorage} from "../../src/Vault/VaultCore/VaultStorage.sol";
+import "forge-std/Test.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ISwapRouter} from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import {IQuoter} from "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
+import {SwapHelper} from "../../src/Vault/VaultCore/SwapHelper.sol";
+import {VaultStorage} from "../../src/Vault/VaultCore/VaultStorage.sol";
 
-// contract SwapHelperTest is Test {
-//     // --- Mainnet Addresses ---
-//     address constant DAI = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
-//     address constant USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
-//     address constant WBTC = 0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6;
-//     address constant PAXG = 0x553d3D295e0f695B9228246232eDF400ed3560B5;
+contract SwapHelperTest is Test {
+    // --- Mainnet Addresses ---
+    address constant DAI = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
+    address constant USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
+    address constant WBTC = 0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6;
+    address constant PAXG = 0x553d3D295e0f695B9228246232eDF400ed3560B5;
 
-//     ISwapRouter constant UNISWAP_ROUTER = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
-//     IQuoter constant UNISWAP_QUOTER = IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
+    ISwapRouter constant UNISWAP_ROUTER = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+    IQuoter constant UNISWAP_QUOTER = IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
 
-//     address testUser = makeAddr("user");
+    address testUser = makeAddr("user");
 
-//     SwapHelperWrapper swapHelper;
+    SwapHelperWrapper swapHelper;
 
-//     function setUp() public {
-//         vm.createSelectFork(
-//             "https://rpc.ankr.com/polygon/cea6f21cc6dc0df6ac58a42690aa0581b0e1981309f47385e0257994874b121e"
-//         );
-//         swapHelper = new SwapHelperWrapper(
-//             DAI,
-//             PAXG,
-//             WBTC,
-//             USDC,
-//             address(0),
-//             address(0),
-//             address(this),
-//             address(UNISWAP_ROUTER),
-//             address(UNISWAP_QUOTER)
-//         );
+    function setUp() public {
+        vm.createSelectFork(
+            "https://rpc.ankr.com/polygon/cea6f21cc6dc0df6ac58a42690aa0581b0e1981309f47385e0257994874b121e"
+        );
+        swapHelper = new SwapHelperWrapper(
+            DAI,
+            PAXG,
+            WBTC,
+            USDC,
+            address(0),
+            address(0),
+            address(this),
+            address(UNISWAP_ROUTER),
+            address(UNISWAP_QUOTER)
+        );
 
-//         // Fund testUser with DAI for testing
-//         deal(DAI, testUser, 10_000e18);
-//         deal(USDC, testUser, 10_000e6);
-//         deal(WBTC, testUser, 10e8); // 10 WBTC
+        // Fund testUser with DAI for testing
+        deal(DAI, testUser, 10_000e18);
+        deal(USDC, testUser, 10_000e6);
+        deal(WBTC, testUser, 10e8); // 10 WBTC
 
-//         // Impersonate an account that already holds tokens (DAI whale)
-//         address daiWhale = 0x4bf61e01450574dC15229542A1EF785af3Dc1Bb3;
-//         vm.startPrank(daiWhale);
-//         IERC20(DAI).transfer(testUser, 100000e18);
-//         vm.stopPrank();
+        // Impersonate an account that already holds tokens (DAI whale)
+        address daiWhale = 0x7A81BBFe9516f81e49c163364264e53b71a49298;
+        vm.startPrank(daiWhale);
+        IERC20(DAI).transfer(testUser, 100000e18);
+        vm.stopPrank();
 
-//         address usdcWhale = 0x1347378B1d0Eb69d3462e09b3dFa2Fe28ebE74eC;
-//         vm.startPrank(usdcWhale);
-//         IERC20(USDC).transfer(testUser, 100000e6);
-//         vm.stopPrank();
+        address usdcWhale = 0x1347378B1d0Eb69d3462e09b3dFa2Fe28ebE74eC;
+        vm.startPrank(usdcWhale);
+        IERC20(USDC).transfer(testUser, 100000e6);
+        vm.stopPrank();
 
-//         // WBTC whale
-//         address wbtcWhale = 0x0AFF6665bB45bF349489B20E225A6c5D78E2280F;
-//         vm.startPrank(wbtcWhale);
-//         IERC20(WBTC).transfer(testUser, 10e8);
-//         vm.stopPrank();
-//     }
+        // WBTC whale
+        address wbtcWhale = 0x0AFF6665bB45bF349489B20E225A6c5D78E2280F;
+        vm.startPrank(wbtcWhale);
+        IERC20(WBTC).transfer(testUser, 10e8);
+        vm.stopPrank();
+    }
 
-//     function testSwapFromDAIToUSDC() public {
-//         uint256 amountIn = 100e18;
-//         vm.startPrank(testUser);
+    function testSwapFromDAIToUSDC() public {
+        uint256 amountIn = 100e18;
+        vm.startPrank(testUser);
 
-//         IERC20(DAI).transfer(address(swapHelper), amountIn);
-//         swapHelper.swapFromDAI(USDC, amountIn, testUser);
+        IERC20(DAI).transfer(address(swapHelper), amountIn);
+        swapHelper.swapFromDAI(USDC, amountIn, testUser);
 
-//         uint256 usdcBalance = IERC20(USDC).balanceOf(testUser);
-//         assertGt(usdcBalance, 0, "USDC balance should increase");
-//         vm.stopPrank();
-//     }
+        uint256 usdcBalance = IERC20(USDC).balanceOf(testUser);
+        assertGt(usdcBalance, 0, "USDC balance should increase");
+        vm.stopPrank();
+    }
 
-//     function testSwapFromDAIToWBTC() public {
-//         uint256 amountIn = 100e18;
-//         vm.startPrank(testUser);
+    function testSwapFromDAIToWBTC() public {
+        uint256 amountIn = 100e18;
+        vm.startPrank(testUser);
 
-//         IERC20(DAI).transfer(address(swapHelper), amountIn);
-//         swapHelper.swapFromDAI(WBTC, amountIn, testUser);
+        IERC20(DAI).transfer(address(swapHelper), amountIn);
+        swapHelper.swapFromDAI(WBTC, amountIn, testUser);
 
-//         uint256 wbtcBalance = IERC20(WBTC).balanceOf(testUser);
-//         assertGt(wbtcBalance, 0, "WBTC balance should increase");
-//         vm.stopPrank();
-//     }
+        uint256 wbtcBalance = IERC20(WBTC).balanceOf(testUser);
+        assertGt(wbtcBalance, 0, "WBTC balance should increase");
+        vm.stopPrank();
+    }
 
-//     function testSwapToDAIFromUSDC() public {
-//         uint256 amountIn = 100e6;
-//         vm.startPrank(testUser);
+    function testSwapToDAIFromUSDC() public {
+        uint256 amountIn = 100e6;
+        vm.startPrank(testUser);
 
-//         IERC20(USDC).transfer(address(swapHelper), amountIn);
-//         swapHelper.swapToDAI(USDC, amountIn, testUser);
+        IERC20(USDC).transfer(address(swapHelper), amountIn);
+        swapHelper.swapToDAI(USDC, amountIn, testUser);
 
-//         uint256 daiBalance = IERC20(DAI).balanceOf(testUser);
-//         assertGt(daiBalance, 0, "DAI balance should increase");
-//         vm.stopPrank();
-//     }
+        uint256 daiBalance = IERC20(DAI).balanceOf(testUser);
+        assertGt(daiBalance, 0, "DAI balance should increase");
+        vm.stopPrank();
+    }
 
-//     function testSwapForExactDAIFromWBTC() public {
-//         uint256 amountOut = 50e18;
-//         uint256 amountInMax = 1e8; // 1 WBTC max
+    function testSwapForExactDAIFromWBTC() public {
+        uint256 amountOut = 50e18;
+        uint256 amountInMax = 1e8; // 1 WBTC max
 
-//         vm.startPrank(testUser);
+        vm.startPrank(testUser);
 
-//         IERC20(WBTC).transfer(address(swapHelper), amountInMax);
+        IERC20(WBTC).transfer(address(swapHelper), amountInMax);
 
-//         uint256 lastBalance = IERC20(DAI).balanceOf(testUser);
+        uint256 lastBalance = IERC20(DAI).balanceOf(testUser);
 
-//         swapHelper.swapForExactDAI(WBTC, amountOut, amountInMax, testUser);
+        swapHelper.swapForExactDAI(WBTC, amountOut, amountInMax, testUser);
 
-//         uint256 newBalance = IERC20(DAI).balanceOf(testUser);
-//         assertEq(newBalance - lastBalance, amountOut, "DAI balance should match exact output");
+        uint256 newBalance = IERC20(DAI).balanceOf(testUser);
+        assertEq(newBalance - lastBalance, amountOut, "DAI balance should match exact output");
 
-//         vm.stopPrank();
-//     }
-// }
+        vm.stopPrank();
+    }
+}
 
-// // Simple wrapper contract inheriting SwapHelper for testing
-// contract SwapHelperWrapper is SwapHelper {
-//     constructor(
-//         address dai,
-//         address paxg,
-//         address wbtc,
-//         address usdc,
-//         address dnm,
-//         address priceFeed,
-//         address coreAddr,
-//         address uniswapRouter,
-//         address uniswapQuoter
-//     )
-//         VaultStorage(VaultStorage.InitParams({
-//                 dai: dai,
-//                 paxg: paxg,
-//                 wbtc: wbtc,
-//                 usdc: usdc,
-//                 dnm: dnm,
-//                 priceFeed: priceFeed,
-//                 coreContract: coreAddr,
-//                 uniswapRouter: uniswapRouter,
-//                 uniswapQuoter: uniswapQuoter,
-//                 admin1: address(0),
-//                 admin2: address(0),
-//                 admin3: address(0),
-//                 feeReceiver: address(0)
-//             }))
-//     {}
+// Simple wrapper contract inheriting SwapHelper for testing
+contract SwapHelperWrapper is SwapHelper {
+    constructor(
+        address dai,
+        address paxg,
+        address wbtc,
+        address usdc,
+        address dnm,
+        address priceFeed,
+        address coreAddr,
+        address uniswapRouter,
+        address uniswapQuoter
+    )
+        VaultStorage(VaultStorage.InitParams({
+                dai: dai,
+                paxg: paxg,
+                wbtc: wbtc,
+                usdc: usdc,
+                arc: dnm,
+                priceFeed: priceFeed,
+                coreContract: coreAddr,
+                uniswapRouter: uniswapRouter,
+                uniswapQuoter: uniswapQuoter,
+                admin1: address(0),
+                admin2: address(0),
+                admin3: address(0),
+                feeReceiver: address(0)
+            }))
+    {}
 
-//     function swapFromDAI(address tokenOut, uint256 amountIn, address to) external {
-//         _swapFromDAI(tokenOut, amountIn, to);
-//     }
+    function swapFromDAI(address tokenOut, uint256 amountIn, address to) external {
+        _swapFromDai(tokenOut, amountIn, to);
+    }
 
-//     function swapToDAI(address tokenIn, uint256 amountIn, address to) external {
-//         _swapToDAI(tokenIn, amountIn, to);
-//     }
+    function swapToDAI(address tokenIn, uint256 amountIn, address to) external {
+        _swapToDai(tokenIn, amountIn, to);
+    }
 
-//     function swapForExactDAI(address tokenIn, uint256 amountOut, uint256 amountInMax, address to) external {
-//         _swapForExactDAI(tokenIn, amountOut, amountInMax, to);
-//     }
-// }
+    function swapForExactDAI(address tokenIn, uint256 amountOut, uint256 amountInMax, address to) external {
+        _swapForExactDai(tokenIn, amountOut, amountInMax, to);
+    }
+}
---
+++ test/Vault/VaultFork.t.sol
@@ -0,0 +1,181 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import "forge-std/Test.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MultiAssetVault} from "../../src/Vault/Vault.sol";
+import {VaultStorage} from "../../src/Vault/VaultCore/VaultStorage.sol";
+import {MockToken} from "../mocks/MockToken.sol";
+
+contract MultiAssetVaultForkSwapTest is Test {
+    // --- Polygon Mainnet Tokens ---
+    address constant DAI = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
+    address constant USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
+    address constant WBTC = 0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6;
+    address constant PAXG = 0x553d3D295e0f695B9228246232eDF400ed3560B5;
+
+    // --- Uniswap Router + Quoter ---
+    address constant UNISWAP_ROUTER = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    address constant UNISWAP_QUOTER = 0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6;
+
+    // --- Test participants ---
+    address internal user = makeAddr("user");
+    address internal admin1 = makeAddr("admin1");
+    address internal admin2 = makeAddr("admin2");
+    address internal admin3 = makeAddr("admin3");
+    address internal feeReceiver = makeAddr("feeReceiver");
+    address internal coreContract = makeAddr("core");
+
+    MultiAssetVault internal vault;
+    MockToken internal dnm; // ARC substitute
+
+    function setUp() public {
+        // Polygon fork
+        vm.createSelectFork(
+            "https://rpc.ankr.com/polygon/cea6f21cc6dc0df6ac58a42690aa0581b0e1981309f47385e0257994874b121e"
+        );
+
+        // Deploy local DNM (ARC) mock token
+        dnm = new MockToken(address(this), 0);
+
+        // Initialize vault params with live assets
+        VaultStorage.InitParams memory params = VaultStorage.InitParams({
+            dai: DAI,
+            paxg: PAXG,
+            wbtc: WBTC,
+            usdc: USDC,
+            arc: address(dnm),
+            priceFeed: address(0),
+            coreContract: coreContract,
+            uniswapRouter: UNISWAP_ROUTER,
+            uniswapQuoter: UNISWAP_QUOTER,
+            admin1: admin1,
+            admin2: admin2,
+            admin3: admin3,
+            feeReceiver: feeReceiver
+        });
+
+        vault = new MultiAssetVault(params);
+
+        // Enable swaps
+        vm.prank(admin1);
+        vault.updateSwapEnabled(true);
+
+        // Fund the test user with real mainnet DAI
+        address daiWhale = 0x7A81BBFe9516f81e49c163364264e53b71a49298;
+        vm.startPrank(daiWhale);
+        IERC20(DAI).transfer(user, 5_000e18);
+        vm.stopPrank();
+
+        vm.prank(admin1);
+        vault.addFeeTier(100, 0);
+        vm.prank(admin1);
+        vault.addFeeTier(200, 500e18);
+        vm.prank(admin1);
+        vault.addFeeTier(300, 1000e18);
+    }
+
+    /// ----------------------------------------------------
+    /// Test 1 — Deposit triggers real swaps via Vault
+    /// ----------------------------------------------------
+    function testDepositPerformsLiveSwaps() public {
+        uint256 depositAmount = 1000e18;
+
+        vm.startPrank(user);
+        IERC20(DAI).approve(address(vault), depositAmount);
+
+        uint256 daiBefore = IERC20(DAI).balanceOf(address(vault));
+        uint256 paxgBefore = IERC20(PAXG).balanceOf(address(vault));
+        uint256 wbtcBefore = IERC20(WBTC).balanceOf(address(vault));
+
+        vault.deposit(depositAmount);
+
+        vm.stopPrank();
+
+        uint256 daiAfter = IERC20(DAI).balanceOf(address(vault));
+        uint256 paxgAfter = IERC20(PAXG).balanceOf(address(vault));
+        uint256 wbtcAfter = IERC20(WBTC).balanceOf(address(vault));
+
+        // Verify swaps occurred
+        assertGt(paxgAfter, paxgBefore, "Vault PAXG should increase");
+        assertGt(wbtcAfter, wbtcBefore, "Vault WBTC should increase");
+        assertGt(daiAfter, 0, "Vault should retain some DAI portion");
+    }
+
+    /// ----------------------------------------------------
+    /// Test 2 — Redeem converts back to DAI
+    /// ----------------------------------------------------
+    function testRedeemPerformsLiveSwaps() public {
+        uint256 depositAmount = 1500e18;
+
+        // Deposit first (generates DNM)
+        vm.startPrank(user);
+        IERC20(DAI).approve(address(vault), depositAmount);
+        vault.deposit(depositAmount);
+        vm.stopPrank();
+
+        // Mint mock DNM manually to simulate ownership
+        dnm.mint(user, depositAmount);
+
+        // Check user DAI before redeem
+        uint256 daiBefore = IERC20(DAI).balanceOf(user);
+
+        vm.startPrank(user);
+        dnm.approve(address(vault), depositAmount);
+        vault.redeem(depositAmount);
+        vm.stopPrank();
+
+        uint256 daiAfter = IERC20(DAI).balanceOf(user);
+        assertGt(daiAfter, daiBefore, "User DAI balance should increase after redeem");
+    }
+
+    /// ----------------------------------------------------
+    /// Test 3 — Zero deposit reverts
+    /// ----------------------------------------------------
+    function testZeroDepositReverts() public {
+        vm.startPrank(user);
+        vm.expectRevert(bytes("Deposit amount must be > 0"));
+        vault.deposit(0);
+        vm.stopPrank();
+    }
+
+    function testWithdrawDaiTriggersExactDaiSwaps() public {
+        uint256 depositAmount = 2000e18;
+
+        // Step 1: Deposit DAI to create PAXG/WBTC reserves
+        vm.startPrank(user);
+        IERC20(DAI).approve(address(vault), depositAmount);
+        vault.deposit(depositAmount);
+        vm.stopPrank();
+
+        // Step 2: Record pre-withdrawal balances
+        uint256 daiBeforeVault = IERC20(DAI).balanceOf(address(vault));
+        uint256 paxgBefore = IERC20(PAXG).balanceOf(address(vault));
+        uint256 wbtcBefore = IERC20(WBTC).balanceOf(address(vault));
+        uint256 daiBeforeCore = IERC20(DAI).balanceOf(coreContract);
+
+        // Step 3: Withdraw more DAI than vault holds to trigger swaps
+        uint256 withdrawAmount = daiBeforeVault + 200e18;
+
+        vm.startPrank(coreContract);
+        vault.withdrawDai(withdrawAmount);
+        vm.stopPrank();
+
+        // Step 4: Record after-withdrawal balances
+        uint256 daiAfterVault = IERC20(DAI).balanceOf(address(vault));
+        uint256 paxgAfter = IERC20(PAXG).balanceOf(address(vault));
+        uint256 wbtcAfter = IERC20(WBTC).balanceOf(address(vault));
+        uint256 daiAfterCore = IERC20(DAI).balanceOf(coreContract);
+
+        // Assertions
+        // Vault DAI should decrease
+        assertLt(daiAfterVault, daiBeforeVault, "Vault DAI decreased after withdrawal");
+
+        // PAXG and WBTC should decrease (sold for DAI)
+        assertLt(paxgAfter, paxgBefore, "Vault sold some PAXG");
+        assertLt(wbtcAfter, wbtcBefore, "Vault sold some WBTC");
+
+        // Core contract should receive DAI
+        assertGt(daiAfterCore, daiBeforeCore, "Core contract received DAI");
+    }
+}
