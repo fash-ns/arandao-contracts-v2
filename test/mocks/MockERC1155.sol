ae1074f420c95d9570efefa359b2bd53ec76632f
Sajad <SajadSolidity@gmail.com>
2025-11-04 13:03:41 +0330

    updated

--- test/mocks/MockERC1155.sol
+++ test/mocks/MockERC1155.sol
@@ -4,10 +4,17 @@ pragma solidity ^0.8.27;
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 contract MockERC1155 is ERC1155 {
-    constructor(string memory uri_) ERC1155(uri_) {}
+    address public  ownerAddress;
+    constructor(string memory uri_) ERC1155(uri_) {
+        ownerAddress = msg.sender;
+    }
 
     /// @notice Mint tokens to an address
     function mint(address to, uint256 id, uint256 amount, bytes memory data) external {
         _mint(to, id, amount, data);
+    }
+
+    function owner() external view returns (address) {
+        return ownerAddress;
     }
 }
